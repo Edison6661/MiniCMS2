920e42e8e2d9690e9c4a3514e422ab1d680d22c5
Edison6661 <edisonhuk1717@gmail.com>
2025-06-14 17:39:09 +0300

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,15 @@
+// backend/config/db.js
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql', // або 'postgres' якщо використовуєш PostgreSQL
+    logging: false,
+  }
+);
+
+module.exports = sequelize;
